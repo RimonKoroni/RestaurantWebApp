68e37ffb9439575c790586b45ce524e8ade4e37a
Rimon <rimonkoronig@gmail.com>
2017-01-10 10:13:47 +0200

    test 2

--- resources/views/pages/statistics.blade.php
+++ resources/views/pages/statistics.blade.php
@@ -11,9 +11,6 @@
 	    <div id="breadcrumb"> <a href="{{ url('home') }}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i>{{ trans('app.home') }}</a> <a href="#" class="current">{{ trans('app.statistics') }}</a> </div>
 	  </div>
 	<!--End-breadcrumbs-->
-
-
-
 @stop
 
 @section('script')
